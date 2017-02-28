0cba47b9a427d29f5e8cef745bb5c10c6d46d46d
barnhousestudios <barnhousestudiosofficial@gmail.com>
2017-02-28 11:40:56 -0500

    Add files via upload

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,11 @@
-function removeDummy() {
- var elem = document.getElementById('coverimg');
- elem.src="about.png"
+function slideshow() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(slideshow, 3500); // Change image every 2 seconds
 }
